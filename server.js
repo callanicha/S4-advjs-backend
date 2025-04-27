25847f587359c1301bc323a876adef95fb72fcb1
natthanicha <natthanicha.vongjarit@epita.fr>
2025-04-27 17:27:27 +0200

    fix cors

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // --- Middlewares ---
-app.use(cors());
+app.use(cors({
+    origin: "https://s4-advjs-project.netlify.app",
+    credentials: true
+  }));
 app.use(express.json()); 
 
 // --- Fake in-memory storage (for demo) ---
